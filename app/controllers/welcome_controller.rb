74d6210d93c18b68097a5781c14f989ff22958c9
NotGrm <notgrm@gmail.com>
2012-06-06 16:25:43 +0200

    Fix dish add error, and filter on /admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  skip_before_filter :require_table_number, :only => [:configure]
+  before_filter :require_table_number, :fetch_menu, :except => [:configure]
   def index
   end
 
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,5 +5,5 @@ class Dish < ActiveRecord::Base
   has_many :assignments
   has_many :menus, :through => :assignments
 
-  attr_accessible :name, :priceHT, :tva, :description
+  attr_accessible :name, :priceHT, :tva, :description, :category_id, :country_id
 end
