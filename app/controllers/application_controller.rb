ce8cf9370a65f63e3913e7df1c0169dea055fbc0
pierg3ek <pierrick.rouxel@gmail.com>
2012-06-06 18:51:41 +0200

    Changement gem bootstrap + corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,11 @@ class ApplicationController < ActionController::Base
     unless identified?
       redirect_to welcome_configure_path
     end
+    @table_number = cookies[:table_number]
   end
 
   def identified?
-    !!cookies[:table_number]
+    !cookies[:table_number].blank?
   end
 
   def fetch_menu
